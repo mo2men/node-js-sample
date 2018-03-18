6f02f4e2f590c106f8a626ae899a78d1a750f256
mo2men <friendtobehappy@gmail.com>
2018-03-17 20:56:37 -0700

    styles step back as its not ready yet

--- public/advising/script.js
+++ public/advising/script.js
@@ -34,7 +34,7 @@ function loadData() {
 	    }
 	});
 
-	createVisualisation(meatings);
+	//createVisualisation(meatings);
 
     };
 
@@ -47,61 +47,4 @@ function loadData() {
  Visualisation
  */
 
-function createVisualisation(meatings) {
 
-    /*
-     Options
-     */
-
-
-    var width = 800;
-    var hieght = width/2;
-    var Square_side = 20;
-    var color = d3.scale.category20();
-
-    var meetingsStr=[];
-    meetingsStr = meetings.forEach(function(meeting){
-	return meeting.code+"\n"+meeting.type;
-    });
-    console.log(meetingStr);
-    /*
-     Creating an empty svg element
-     */
-    var svg = d3.select("#root").append("svg")
-	.attr("width", width)
-	.attr("height", height);
-
-    /*
-     Instantiate the graph layout using d3
-    */
-
-    var gaticule = d3.geo.graticule();
-    var projection = d3.geo.equirectanguler().translate([0, 0]).scale(width / 2 / Math.Pi );
-    var path =d3.geo.path().projection(projection);
-
-    var outterg =svg.append("g")
-	.attr("transfrom","translate("+width / 2 + "," + height / 2 +")");
-
-    var g = outterg.append("g").attr("id","innerg");
-
-    g.append("defs").append("path")
-	.datum({type: "Sphere"})
-	.attr("id","sphere")
-	.attr("d",path);
-
-    g.append("use")
-	.attr("class","stroke")
-	.attr("xlink:href","#sphere");
-
-    g.append("use")
-	.attr("class","fill")
-	.attr("xlink:href","#sphere");
-
-
-    g.append("path")
-	.datum(graticule)
-	.attr("class","graticule")
-	.attr("d","path");
-
-
-}
